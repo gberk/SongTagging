e6d8f23a1a4aee910f87bd52ef0fe196897dc72e
gberk <gavin.berkowitz.bu@gmail.com>
2013-09-03 20:18:05 -0400

    More setup
    Added the mariasql node package

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var testpage = require('./routes/test')
 
 var app = express();
 
@@ -32,6 +33,8 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+
+app.get('/test', testpage.test);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
---
+++ routes/test.js
@@ -0,0 +1,6 @@
+
+//Get the test page
+
+exports.test = function(req,res){
+	res.render('test' ,{title: "aint that some shit"});
+}; 
