86839238f05d2b7f821eaa60a4c93016afc66393
gberk <gavin.berkowitz.bu@gmail.com>
2013-09-11 20:31:28 -0400

    Extending database testing

--- test/database.js
+++ test/database.js
@@ -1,14 +1,30 @@
 var assert = require("assert");
 var mongoose = require("mongoose");
+var should = require("should");
 
 var testConnection = 'mongodb://localhost:27017/test_database'
+var connection;
 
-describe('Database functions: ' ,function(){
+describe('Database' ,function(){
+
+	afterEach(function(){
+		mongoose.connection.close();
+	})
+
 	describe('Given a connection string',function(){
 		it('should connect to that database',function(done){
-			mongoose.connection.on('open',done);
+			mongoose.connection.on('open',function(){
+				mongoose.connection.close();
+				done();
+			});
 			
 			mongoose.connect(testConnection);
 		});
+	});
+
+	describe('When dropping a database',function(){
+		it('should drop that database',function(){
+			
+		})
 	});
 });
--- test/music.js
+++ test/music.js
@@ -1,14 +1,30 @@
-var assert = require("assert");
-var mongoose = require("mongoose");
+// var assert = require("assert");
+// var mongoose = require("mongoose");
 
-var Library = require("../controllers/library");
-describe('Library',function(){
-	describe('adding a song',function(){
-		it('should add a new song entity to the database');
-	});
-});
+// var testConnection = 'mongodb://localhost:27017/test_database'
+
+// var Library = require("../controllers/library");
+// describe('Library',function(){
 
 
-beforeEach(function(){
-	console.log('Running beforeEach function');
-})
+// 	before(function(){
+// 		mongoose.connect(testConnection);
+// 		mongoose.model(Library.model);
+// 	});
+
+// 	after(function(){
+// 		mongoose.model.drop(function{})
+// 	});
+
+// 	describe('adding a song',function(){
+// 		it('should add a new song entity to the database',function(){
+// 			assert.equal(1,1);
+// 		});
+// 	});
+
+// 	describe('adding a song',function(){
+// 		it('should add a new song entity to the database',function(){
+// 			assert.equal(1,1);
+// 		});
+// 	});
+// });
