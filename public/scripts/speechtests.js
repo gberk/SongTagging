55488e6972a869f37437fb24802b93358b74fbd6
Gavin Berkowitz <gavin.berkowitz.bu@gmail.com>
2013-09-24 20:47:46 -0400

    Speech to tracks!

--- public/scripts/speechtests.js
+++ public/scripts/speechtests.js
@@ -1,23 +1,42 @@
 function searchEntered(speechEvent){
     if(speechEvent.target.value != ""){
         $('#speechInput').unbind("focus");
-        var searchParam = speechEvent.target.value.replace(/\s+/g,'');
-        var url = '/api/search/' + searchParam;
-        console.log("searching " + url);
-        $.ajax({
-            url:url
-        }).done(function(data){
-            console.log(data);
-        });
+        //var searchParam = speechEvent.target.value.replace(/\s+/g,'');
+        spotifyTrackSearch(speechEvent.target.value);
     }
 }
 
+function spotifyTrackSearch(query){
+    var url = 'http://ws.spotify.com/search/1/track.json?q=' + query;
+        $.ajax({
+            url:url,
+            dataType:"json"
+        }).done(function(data){
+            lastResult = data;
+            console.log(data);
+            appendSpotifyPlayer(data.tracks[0].href)
+        });
+}
+
+function appendSpotifyPlayer(trackId){
+    var spotifyPlayer = "<iframe src=\"https://embed.spotify.com/?uri="+ trackId + "\" width=\"300px\" height=\"80px\" frameborder=\"0px\" allowtransparency=\"true\"/>";
+    $(".result").append(spotifyPlayer);
+    $('.result').show();
+}
+
+var lastResult = "blah";
+
+$(function(){
+    $('.result').hide();
+    $('#speechInput').on("focus",searchEntered);
+    //spotifyTrackSearch("The Mother We Share");
+});
+
+
+/*
 var recognition = new webkitSpeechRecognition();
 recognition.continuous = false;
 recognition.interimResults = false;
 
 recognition.onEnd = searchEntered;
-
-$(function(){
-    $('#speechInput').on("focus",searchEntered);
-});
+*/
