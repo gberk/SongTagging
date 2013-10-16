ba2408b93d20c13248967e0eb1aaecc229dccce2
gberk <gavin.berkowitz.bu@gmail.com>
2013-10-16 11:11:46 -0400

    Removed my own javascript classes to try and get heroku working

--- app.js
+++ app.js
@@ -7,19 +7,7 @@ var express = require('express'),
 	routes = require('./controllers'),
 	http = require('http'),
 	path = require('path'),
-	mongoose = require('mongoose'),
-	User = require('./controllers/User')
-	Library = require('./controllers/Library'),
-    jade = require('jade');
-
-mongoose.connect('mongodb://localhost:27017/test');
-
-var db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function callback () {
-		console.log('Database opened');
-});
+  jade = require('jade');
 
 var app = express();
 
@@ -53,19 +41,6 @@ app.get('/playsong', function(req,res){
     res.render('playsong');
 });
 
-app.get('/users', User.all);
-
-app.post('/users/:name/:pass', User.createUser);
-
-app.get('/api/songList', Library.songList);
-app.post('/api/addSong', Library.addSong);
-
-app.get('/api/search/', function(req,res){
-    Spotify.search(function(err,val){
-        console.log("Search spotify and found " + val);
-        res.end(val);
-    });
-});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
